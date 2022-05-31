4ce13eb59b63bb25d48c25373d00124b89ed46ef
nadinecornelio <94148276+nadinecornelio@users.noreply.github.com>
2022-05-30 21:21:37 -0300

    Add files via upload

---
+++ TesteTipos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testetipos;
+
+/**
+ *
+ * @author 1124966
+ */
+public class TesteTipos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String valor = "30.5";
+        float idade = Float.parseFloat(valor);
+        System.out.printf("%.3f \n", idade);
+    }   
+    
+}
